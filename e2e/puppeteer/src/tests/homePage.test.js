ef46a612d786a78a938a6691ac5a14a403631dba
Nicole Stock <nicole.stock@jhuapl.edu>
2025-04-01 13:44:36 -0400

    Comment out test that is failing b/c we have commented out the code that allows the test to pass

--- e2e/puppeteer/src/tests/homePage.test.js
+++ e2e/puppeteer/src/tests/homePage.test.js
@@ -1,9 +1,13 @@
 const VERSION_LINK_REGEX = /https:\/\/github.com\/dandi\/dandi-archive\/commit\/[0-9a-f]{5,40}/;
 
 describe('home page stats', () => {
-  it('checks version link', async () => {
-    /* eslint-disable-next-line no-undef */
-    const versionLink = await page.evaluate(() => document.querySelector('.version-link').getAttribute('href'));
-    expect(versionLink).toMatch(VERSION_LINK_REGEX);
-  });
+  // TODO: Re-enable test when <a class="version-link" > is uncommented from DandiFooter.vue
+  //    The commented out div displays the version, which is pulled from git tags. In EMBER-DANDI,
+  //    are not currently using tags, so the version displays as unknown.
+
+  // it('checks version link', async () => {
+  //   /* eslint-disable-next-line no-undef */
+  //   const versionLink = await page.evaluate(() => document.querySelector('.version-link').getAttribute('href'));
+  //   expect(versionLink).toMatch(VERSION_LINK_REGEX);
+  // });
 });
