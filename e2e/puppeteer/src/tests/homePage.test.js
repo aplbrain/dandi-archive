f67f18fdfdf82806a255237274dc4b32b0878b85
Nicole Stock <nicole.stock@jhuapl.edu>
2025-04-01 14:49:16 -0400

    trying again... can't test locally

--- e2e/puppeteer/src/tests/homePage.test.js
+++ e2e/puppeteer/src/tests/homePage.test.js
@@ -1,16 +1,15 @@
 const VERSION_LINK_REGEX = /https:\/\/github.com\/dandi\/dandi-archive\/commit\/[0-9a-f]{5,40}/;
 
 describe('home page stats', () => {
-  // TODO: Re-set test when <a class="version-link" > is uncommented from DandiFooter.vue
-  //    The commented out div displays the version, which is pulled from git tags. In EMBER-DANDI,
-  //    are not currently using tags and therefore do not have a version, so we've commented out the link .
-
   it('checks version link', async () => {
-    /* eslint-disable-next-line no-undef */
-    const versionLink = await page.evaluate(() => document.querySelector('.version-link').getAttribute('href'));
-    if (versionLink == undefined) {
-      VERSION_LINK_REGEX = undefined;
+    let version = process.env.VITE_APP_VERSION;
+    if (version !== 'unknown') {
+      /* eslint-disable-next-line no-undef */
+      const versionLink = await page.evaluate(() => document.querySelector('.version-link').getAttribute('href'));
+      expect(versionLink).toMatch(VERSION_LINK_REGEX);
+    } else {
+      const versionLinkEl = await page.evaluate(() => document.querySelector('.version-link'));
+      expect(versionLinkEl).toMatch(undefined);
     }
-    expect(versionLink).toMatch(VERSION_LINK_REGEX);
   });
 });
