edb369025b432e47ce60742fda09fb699067f38d
Nicole Stock <nicole.stock@jhuapl.edu>
2025-04-01 15:10:56 -0400

    Skip test

--- e2e/puppeteer/src/tests/homePage.test.js
+++ e2e/puppeteer/src/tests/homePage.test.js
@@ -1,15 +1,11 @@
 const VERSION_LINK_REGEX = /https:\/\/github.com\/dandi\/dandi-archive\/commit\/[0-9a-f]{5,40}/;
 
 describe('home page stats', () => {
-  it('checks version link', async () => {
-    let version = process.env.VITE_APP_VERSION;
-    if (version !== 'unknown') {
-      /* eslint-disable-next-line no-undef */
-      const versionLink = await page.evaluate(() => document.querySelector('.version-link').getAttribute('href'));
-      expect(versionLink).toMatch(VERSION_LINK_REGEX);
-    } else {
-      const versionLinkEl = await page.evaluate(() => document.querySelector('.version-link'));
-      expect(versionLinkEl).toMatch(undefined);
-    }
+  // Skip test until we setup git tags and releases to control and enable version
+  /* eslint-disable-next-line jest/no-disabled-tests */
+  it.skip('checks version link', async () => {
+    /* eslint-disable-next-line no-undef */
+    const versionLink = await page.evaluate(() => document.querySelector('.version-link').getAttribute('href'));
+    expect(versionLink).toMatch(VERSION_LINK_REGEX);
   });
 });
