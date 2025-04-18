618cd3de0a0b9de55fdcca42a3fc19fc51f667cf
Nicole Stock <nicole.stock@jhuapl.edu>
2025-04-18 16:52:22 -0400

    skip version test until we release

--- e2e/tests/homePage.spec.ts
+++ e2e/tests/homePage.spec.ts
@@ -4,7 +4,9 @@ import { clientUrl, dismissCookieBanner } from "../utils.ts";
 const VERSION_LINK_REGEX = /https:\/\/github.com\/dandi\/dandi-archive\/commit\/[0-9a-f]{5,40}/;
 
 test.describe("Home page stats", async () => {
-  test("check version link", async ({ page }) => {
+  // Skip test until we setup git tags and releases to control and enable version
+  /* eslint-disable-next-line jest/no-disabled-tests */
+  test.skip("check version link", async ({ page }) => {
     await page.goto(clientUrl);
     await dismissCookieBanner(page);
     const versionLinkElement = await page.locator(".version-link").first();
