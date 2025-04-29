2a40bf5204666ae99e9ac2db3e25da2f4289c0c4
Sandy Hider <sandy.hider@jhuapl.edu>
2025-04-29 15:35:55 -0400

    updated auth.py to allow posts with authorize_view

--- dandiapi/api/views/auth.py
+++ dandiapi/api/views/auth.py
@@ -63,9 +63,8 @@ NEW_USER_QUESTIONS = QUESTIONS
 # questions for existing users who have no first/last name
 COLLECT_USER_NAME_QUESTIONS = QUESTIONS[:2]
 
-
-# @require_http_methods(['GET', 'POST'])
-@require_http_methods(['GET'])
+# @require_http_methods(['GET'])
+@require_http_methods(['GET', 'POST'])
 def authorize_view(request: HttpRequest) -> HttpResponse:
     """Override authorization endpoint to handle user questionnaire."""
     user: User = request.user
