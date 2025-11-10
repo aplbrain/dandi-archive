c7da6c9f51ba1b1801056adf25d50db78ff9a3d2
Lauren Diaz <lauren.c.d2000@gmail.com>
2025-11-10 11:55:30 -0500

    adjust typehinting and where json is dumped

--- dandiapi/api/mail.py
+++ dandiapi/api/mail.py
@@ -1,12 +1,10 @@
 from __future__ import annotations
 
-import json
 import logging
 from typing import TYPE_CHECKING
 
 from django.conf import settings
 from django.core import mail
-from django.db import models
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 
@@ -115,11 +113,11 @@ def send_registered_notice_email(user: User, socialaccount: SocialAccount):
         connection.send_messages(messages)
 
 
-def build_new_user_messsage(user: User, socialaccount: SocialAccount = None, questionnaire_form: models.JSONField = None):
+def build_new_user_messsage(user: User, socialaccount: SocialAccount = None, questionnaire_form: str = None):
     render_context = {
         **BASE_RENDER_CONTEXT,
         'username': user.username,
-        'questionnaire': json.dumps(questionnaire_form)
+        'questionnaire': questionnaire_form
     }
     # Email sent to the DANDI list when a new user logs in for the first time
     return build_message(
@@ -129,7 +127,7 @@ def build_new_user_messsage(user: User, socialaccount: SocialAccount = None, que
     )
 
 
-def send_new_user_message_email(user: User, socialaccount: SocialAccount, questionnaire_form: models.JSONField):
+def send_new_user_message_email(user: User, socialaccount: SocialAccount, questionnaire_form: str):
     logger.info('Sending new user message for %s to admins', user)
     messages = [build_new_user_messsage(user, socialaccount, questionnaire_form)]
     with mail.get_connection() as connection:
