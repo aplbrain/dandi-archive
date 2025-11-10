752387a8086e921027e00e34e93e531c910a290d
Lauren Diaz <lauren.c.d2000@gmail.com>
2025-11-10 11:42:19 -0500

    testing core changes

--- dandiapi/api/mail.py
+++ dandiapi/api/mail.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
+import json
 import logging
 from typing import TYPE_CHECKING
 
 from django.conf import settings
 from django.core import mail
+from django.db import models
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 
@@ -113,10 +115,11 @@ def send_registered_notice_email(user: User, socialaccount: SocialAccount):
         connection.send_messages(messages)
 
 
-def build_new_user_messsage(user: User, socialaccount: SocialAccount = None):
+def build_new_user_messsage(user: User, socialaccount: SocialAccount = None, questionnaire_form: models.JSONField = None):
     render_context = {
         **BASE_RENDER_CONTEXT,
         'username': user.username,
+        'questionnaire': json.dumps(questionnaire_form)
     }
     # Email sent to the DANDI list when a new user logs in for the first time
     return build_message(
@@ -126,9 +129,9 @@ def build_new_user_messsage(user: User, socialaccount: SocialAccount = None):
     )
 
 
-def send_new_user_message_email(user: User, socialaccount: SocialAccount):
+def send_new_user_message_email(user: User, socialaccount: SocialAccount, questionnaire_form: models.JSONField):
     logger.info('Sending new user message for %s to admins', user)
-    messages = [build_new_user_messsage(user, socialaccount)]
+    messages = [build_new_user_messsage(user, socialaccount, questionnaire_form)]
     with mail.get_connection() as connection:
         connection.send_messages(messages)
 
